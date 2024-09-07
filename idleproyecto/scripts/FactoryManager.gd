6d2f8748556d4a47d1d783cf0b153b4e68e1061b
TheHaput <rasurap666@gmail.com>
2024-09-08 01:27:06 +0200

    He refactorizado el codigo de crear nuevas lineas a Factory Manager

--- idleproyecto/scripts/FactoryManager.gd
+++ idleproyecto/scripts/FactoryManager.gd
@@ -1,6 +1,36 @@
 extends Node
 
-var new_line_price : int = 100
+class_name FactoryManager
+
+var conveyor_scene = preload("res://scenes/conveyor.tscn")
 
 func _ready():
+	
 	pass
+	
+func calculate_last_conveyor_name() -> int:
+	var conveyor_names = []
+	
+	#Guardar en un array los nombres de los conveyors ya existentes
+	for conveyor in get_children():
+		conveyor_names.append(int(str(conveyor.name)))
+		
+	#Coger el numero mas alto del array
+	var last_conveyor_name = conveyor_names.max()
+	return last_conveyor_name
+	
+func create_new_conveyor():
+	#Coje el nodo que corresponde al numero mas alto
+	var last_conveyor_name = calculate_last_conveyor_name()
+	var last_conveyor_node = get_node("%d" %last_conveyor_name)
+	#Instanciamos el conveyor, lo hacemos un nodo.
+	var conveyor_instance = conveyor_scene.instantiate()
+	
+	conveyor_instance.name = str(last_conveyor_name + 1)
+	#Creamos nuevo nodo del conveyor
+	last_conveyor_node.add_sibling(conveyor_instance)
+	#Coje la posicion del ultimo conveyor y su altura multiplicado por la escala y lo suma al conveyor padre para dejar un hueco
+	const conveyor_margin = 5
+	conveyor_instance.position.y = last_conveyor_node.position.y + last_conveyor_node.texture.get_height() * last_conveyor_node.scale.y + conveyor_margin
+	
+	return conveyor_instance
--- idleproyecto/scripts/MainGameLoop.gd
+++ idleproyecto/scripts/MainGameLoop.gd
@@ -1,18 +1,19 @@
 extends Node2D
 
-var conveyor_scene
 var conveyors = []
 var money = 0
 var warehouse = Warehouse.new()
+var factory_manager 
+const new_conveyor_price = 100
+
 
 func _ready():
-	#Carga al escena conveyor
-	conveyor_scene = preload("res://scenes/conveyor.tscn")
+	factory_manager = $FactoryManager
+	
 	for conveyor in get_tree().get_nodes_in_group("Conveyors"):
 		conveyors.append(conveyor)
 	#Boton de comprar nueva linea
-	var button = $GUI/VBoxContainer2/Button 
-	button.connect("pressed", on_buy_line_button_press)
+	$GUI/VBoxContainer2/Button.connect("pressed", on_buy_line_button_press)
 	
 func _on_conveyor_sell_item(item: Item) -> void:
 	money += item.value
@@ -23,37 +24,21 @@ func _on_conveyor_store_item(item: Item) -> void:
 	$GUI/Labels/IronLabel.text = "Iron: %d" %warehouse.items["Iron"]
 
 func on_buy_line_button_press():
-	const new_conveyor_price = 100
+	if !try_take_money():
+		return 
 	
+	var new_conveyor = factory_manager.create_new_conveyor()
+	new_conveyor.connect("sell_item", _on_conveyor_sell_item)
+	new_conveyor.connect("store_item", _on_conveyor_store_item)
+	
+func try_take_money() -> bool:
 	#Si no tiene dinero paramos la funcion con return
 	if money <= new_conveyor_price:
 		print("No tienes dinero")
-		return 
-	
+		return false
+		
 	#Quitamos el dinero
 	money -= new_conveyor_price 
 	$GUI/Labels/MoneyLabel.text = "Money: %d" %money
-	const conveyor_margin = 5
-	#Instanciamos el conveyor, lo hacemos un nodo.
-	var conveyor_instance = conveyor_scene.instantiate()
-	var conveyor_names = []
-	
-	
-	#Guardar en un array los nombres de los conveyors ya existentes
-	for conveyor in $FactoryManager.get_children():
-		conveyor_names.append(int(str(conveyor.name)))
-		
-	
-	
-	#Coger el numero mas alto del array
-	var last_conveyor_name = conveyor_names.max()
-	#Coje el nodo que corresponde al numero mas alto
-	var last_conveyor_node = get_node("FactoryManager/%d" %last_conveyor_name)
-	var conveyor_position = last_conveyor_node.position
-	
-	conveyor_instance.name = str(last_conveyor_name + 1)
-	#Creamos nuevo nodo del conveyor
-	last_conveyor_node.add_sibling(conveyor_instance)
-	#Coje la posicion del ultimo conveyor y su altura multiplicado por la escala y lo suma al conveyor padre para dejar un hueco
-	conveyor_instance.position.y = last_conveyor_node.position.y + last_conveyor_node.texture.get_height() * last_conveyor_node.scale.y + conveyor_margin
+	return true
 	
